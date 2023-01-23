8465b0cf482cd8f2f1857d411d75ae926b1d2a3c
rajpate77725 <92290806+rajpate77725@users.noreply.github.com>
2023-01-24 03:25:11 +0530

    Create generator.js

---
+++ Max_Avatar_Time/generator.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+
+for(let i=0;i<15;i++)
+{
+    const index = "00" +i;
+    const fileIndex= index.substring(index.length-2);    
+    // constraints
+    let valueHigh,valueLow,nLow,nHigh,remain=100000,t;
+    if(i<=5)
+    {
+        nHigh=10;
+        nLow=1;
+        valueHigh = 10;
+        valueLow = 1;
+        remain=20;
+        t=getRandomIntInclusive(1,5);
+    }
+    else
+    {
+        nHigh=10000;    
+        nLow=1;
+        valueHigh = 1000;
+        valueLow = 1;
+        t=getRandomIntInclusive(1,100);
+    }
+    
+    
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+    }
+    function getRandomValue() {
+        return getRandomIntInclusive(valueLow, valueHigh);
+    }
+    let bbb = [];
+    bbb.push(t);
+    for(let i=0;i<t;i++)
+    {
+        let n= getRandomIntInclusive(1,remain-(t-i)+1);
+        remain-=n;
+        let test_case = [];
+        for(let j=0;j<n;j++)
+        {
+            let temp=getRandomIntInclusive(valueLow,valueHigh);
+            test_case.push(temp);
+        }
+        let tests = [n];
+        test_case=test_case.join(" ");
+        let final = [tests,test_case];
+        final=final.join("\n");
+        bbb.push(final);
+        // console.log(final);
+    }
+    
+
+    
+    // test_case = [test_case.length, ...test_case];
+    
+    fs.writeFileSync(`./input/input${fileIndex}.txt`, bbb.join("\n"));
+}
