c4c8c36de74a53354a19bec11a529e3366fb0941
rajpate77725 <92290806+rajpate77725@users.noreply.github.com>
2023-01-24 03:25:41 +0530

    Create program.cpp

---
+++ Max_Avatar_Time/program.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void slidingWindowMaximum(vector<int>nums, vector<int>&ans, int k){
+    int n=nums.size();
+    deque<int>dq;
+    for(int i=0;i<nums.size();i++){
+        if(!dq.empty() && dq.front()==i-k){
+            dq.pop_front();
+        }
+        while(!dq.empty() && nums[dq.back()] < nums[i]){
+            dq.pop_back();
+        }
+        dq.push_back(i);
+        if(i>=k-1){
+            ans.push_back(nums[dq.front()]);
+        }
+    }
+
+    vector<int>rem(k-1);
+    int mx=0;
+    for(int i=0;i<k-1;i++){
+        mx=max(mx,nums[n-i-1]);
+        rem[k-i-2]=mx;
+    }
+    for(int i=0;i<rem.size();i++){
+        ans.push_back(rem[i]);
+    }
+
+}
+
+int main(){
+    cin.tie(0), cout.tie(0),ios_base::sync_with_stdio(false);
+    int t;
+    cin>>t;
+    while(t--){
+        vector<int>nums;
+        int n,k,f;
+        cin>>n>>k>>f;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            nums.push_back(x);
+        }
+        vector<int>ans;
+        slidingWindowMaximum(nums,ans,k+1);
+        // for(int i=0;i<ans.size();i++){
+        //     cout<<ans[i]<<" ";
+        // }cout<<endl;
+        // cout<<" ------------- "<<endl;
+        for(int i=0;i<ans.size();i+=(f+1)){
+            cout<<ans[i]<<" ";
+        }cout<<endl;
+    }
+    return 0;
+}
