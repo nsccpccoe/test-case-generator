2c5b6cecc312661c09ed7cb39c4a519d43189d5b
Vedang Dadape <81163049+Vedang12d@users.noreply.github.com>
2023-01-24 19:03:59 +0530

    Create medium.cpp

---
+++ distributing-fruits/medium.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll NfactorialDivisibleByK(ll n, ll k)
+{
+    for (ll i = 1; i <= n; i++)
+    {
+        k /= gcd(i, k);
+        if (k == 1)
+            break;
+    }
+    return (k == 1);
+}
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, k;
+        cin >> n >> k;
+        // assert(n > 0 && k > 0 && n <= (ll)(1e5) && k <= (ll)(1e14));
+        cout << (NfactorialDivisibleByK(n, k) ? "YES" : "NO") << '\n';
+    }
+}
